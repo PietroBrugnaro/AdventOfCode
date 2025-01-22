a6017847630fcb61de0e9d729e656f2270553ada
PietroBrugnaro <pietro.brugnaro1805@gmail.com>
2025-01-22 12:13:22 +0100

    Initial commit

---
+++ Day01/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    public static void sol1()
+    {
+        if (!File.Exists("input.txt"))
+        {
+            Console.WriteLine("Il file input.txt non è stato trovato.");
+            return;
+        }
+
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+
+
+        string[] lines = File.ReadAllLines("input.txt");
+
+
+        foreach (var line in lines)
+        {
+
+            var item = line.Split(' ');
+
+            if (item.Length == 2)
+            {
+                leftList.Add(int.Parse(item[0]));
+                rightList.Add(int.Parse(item[1]));
+            }
+            else if (item.Length == 5)
+            {
+                leftList.Add(int.Parse(item[3]));
+                rightList.Add(int.Parse(item[4]));
+            }
+        }
+
+
+        leftList.Sort();
+        rightList.Sort();
+
+
+        int distance = 0;
+        for (int i = 0; i < leftList.Count; i++)
+        {
+            distance += Math.Abs(leftList[i] - rightList[i]);
+        }
+
+
+        Console.WriteLine($"\nla distanza è {distance}");
+    }
+    //------Seconda parte dell'esercizio------
+    public static void sol2()
+    {
+       
+        List<int> leftList = new List<int>();
+        List<int> rightList = new List<int>();
+
+        string[] lines = File.ReadAllLines("input.txt");
+
+        foreach (var line in lines)
+        {
+            var item = line.Split(' ');
+
+            if (item.Length == 2)
+            {
+                leftList.Add(int.Parse(item[0]));
+                rightList.Add(int.Parse(item[1]));
+            }
+            else if (item.Length == 5)
+            {
+                leftList.Add(int.Parse(item[3]));
+                rightList.Add(int.Parse(item[4]));
+            }
+        }
+        
+        Dictionary<int, int> rightFrequency = new Dictionary<int, int>();
+
+        foreach (int num in rightList)
+        {
+            if (rightFrequency.ContainsKey(num))
+            {
+                rightFrequency[num]++;
+            }
+            else
+            {
+                rightFrequency[num] = 1;
+            }
+        }
+
+        // Calcola il punteggio di similarità
+        int similarityScore = 0;
+
+        foreach (int leftNum in leftList)
+        {
+            if (rightFrequency.ContainsKey(leftNum))
+            {
+                similarityScore += leftNum * rightFrequency[leftNum];
+            }
+        }
+
+        // Stampa il punteggio di similarità
+        Console.WriteLine($"\npunteggio di similarità è: {similarityScore}");
+    }
+    static void Main()
+    {
+        Program.sol1();
+        Program.sol2();
+    }
+}
---
+++ Day02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+class Program
+{
+    static bool IsReportSafeSol1(string report)
+    {
+
+        int[] levels = report.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                             .Where(s => int.TryParse(s.Trim(), out _))
+                             .Select(int.Parse)
+                             .ToArray();
+
+        if (levels.Length == 0) return false;
+
+        bool increasing = true, decreasing = true;
+
+        for (int i = 1; i < levels.Length; i++)
+        {
+            int diff = Math.Abs(levels[i] - levels[i - 1]);
+            if (diff < 1 || diff > 3)
+                return false;
+            if (levels[i] < levels[i - 1])
+                increasing = false;
+            if (levels[i] > levels[i - 1])
+                decreasing = false;
+        }
+        return increasing || decreasing;
+    }
+    public static void sol1()
+    {
+        if (!File.Exists("input.txt"))
+        {
+            Console.WriteLine("Il file input.txt non è stato trovato.");
+            return;
+        }
+
+        string content = File.ReadAllText("input.txt");
+        string[] reports = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        int safeCount = reports.Count(IsReportSafeSol1);
+        Console.WriteLine($"Numero di report sicuri: {safeCount}");
+
+    }
+    static void Main()
+    {
+        Program.sol1();
+    }
+
+   
+}
